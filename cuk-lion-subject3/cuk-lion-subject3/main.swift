b7dc08b0d7a35eb39fa92e46ecf41abe07a4fcb5
{github HEHEEUN} <{heheeun13@gmail.com}>
2023-04-06 14:32:37 +0900

    Feat: step2 - 접근제어자 추가, pickUpTable 추가 기능

--- cuk-lion-subject3/cuk-lion-subject3/main.swift
+++ cuk-lion-subject3/cuk-lion-subject3/main.swift
@@ -4,15 +4,11 @@
 //
 //  Created by 김희은 on 2023/03/29.
 //
-
 import Foundation
 
-enum Coffee {
-    case americano, cafeLatte
-    case iceTea, vanillaLatte
-    case brunchSet
+enum Coffee{
+    case americano, cafeLatte , iceTea, vanillaLatte , brunchSet
 }
-
 
 struct Person {
     var name: String
@@ -36,16 +32,15 @@ struct Person {
     }
 }
 
-
 struct CoffeeShop{
-    var customer: Person = Person(name: "-", money: 0)
+    var customer: Person = Person(name: "-", money: 0) // ___고객의 이름을 알 수 없음. 해당 고객의 이름을 "-"로 설정해둔 것이 미흡.
     var sales: Int = 0
-    var barista: Person
-    var pickUpTable: Bool
     var menu: [Coffee: Int] = [.americano: 3500, .cafeLatte: 4000, .vanillaLatte: 4500, .brunchSet: 10500]
+    var pickUpTable = [true, false, false]
     
+    var barista: Person
     
-    mutating func order(coffee: Coffee) {
+    public mutating func order(coffee: Coffee) {
         print("===== 안녕하세요. 메뉴를 고르시면 말씀해주세요. =====\n")
         
         if(self.welcomeCustomers(coffee: coffee)) {
@@ -53,14 +48,12 @@ struct CoffeeShop{
             self.serveCoffee()
         }
         print("=========== 감사합니다. 안녕히가세요. ===========\n")
-        pickUpTable = false
     }
     
     
     // 고객 주문 받기
-    mutating func welcomeCustomers(coffee: Coffee) -> Bool {
-        print("\(barista.name)입니다. 주문도와드리겠습니다.")
-        print("\(coffee)주문받았습니다.")
+    private mutating func welcomeCustomers(coffee: Coffee) -> Bool {
+        print("\(barista.name)입니다. \(coffee)주문받았습니다.")
         
         // 주문 불가능한 메뉴일 경우 예외처리
         guard let payment = self.menu[coffee] else {
@@ -71,43 +64,44 @@ struct CoffeeShop{
         if self.customer.buySomething(amount: payment) {
             print("가격은 \(payment)원입니다.")
             self.sales += payment
+
+            return true
         }
-        else {
-            return false
-        }
-        return true
+        return false
     }
     
-    
-    func makeCoffee(){
+    private func makeCoffee(){
         print("음료 제조 중입니다")
     }
     
-
-    mutating func cleanPickUpTable() {
+    private mutating func cleanPickUpTable() {
         print("픽업대를 정리하는 중입니다. 잠시만 기다려주세요.")
-        self.pickUpTable = false
+        for i in 0...2 {
+            self.pickUpTable[i] = false
+        }
     }
     
-    
-    mutating func serveCoffee() {
-        if (pickUpTable) {
-            print("죄송합니다. 픽업대 정리 후 제공해드리겠습니다.")
-            
-            // 픽업대 청소 후 다시 제공
-            self.cleanPickUpTable()
-            self.serveCoffee()
+    private mutating func serveCoffee() {
+        for i in 0...2 {
+            if pickUpTable[i] == false{
+                print("\(i)번 테이블에서 제공해드리겠습니다.")
+                pickUpTable[i] = true
+                break
+            }
+            if (i == 2) && (pickUpTable[2] == true){
+                // 픽업대 청소 후 다시 제공
+                self.cleanPickUpTable()
+                print("0번 테이블에서 제공해드리겠습니다.")
+                pickUpTable[0] = true
+            }
         }
-        else {
-            print("\n\(customer.name)님의 커피가 준비되었습니다. 픽업대에서 가져가주세요.")
-            self.pickUpTable = true
-        }
+        print("\n\(customer.name)님의 커피가 준비되었습니다. 픽업대에서 가져가주세요.")
     }
 }
 
 var misterLee: Person = Person(name: "misterLee", money: 10000)
 var missKim: Person = Person(name: "missKim", money: 5000)
-var mustabucks: CoffeeShop = CoffeeShop(sales: 100000, barista: misterLee, pickUpTable: true)
+var mustabucks: CoffeeShop = CoffeeShop(sales: 100000, barista: misterLee)
 
 mustabucks.customer = missKim
 mustabucks.order(coffee: .brunchSet)
